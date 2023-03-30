c6d0830d6d774c572b9931f02acdfe9ef03de8bd
Ryan Phung <ryan.phung@gmail.com>
2023-03-30 12:09:00 +0800

    Update prompt to make sure ChatGPT does not add the subtitle at the end

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -7,20 +7,20 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const languages = {
-  "zh": "Chinese",
-  "en": "English",
-  "fr": "French",
-  "de": "German",
-  "it": "Italian",
-  "ja": "Japanese",
-  "ko": "Korean",
-  "pt": "Portuguese",
-  "ru": "Russian",
-  "es": "Spanish",
-  "vi": "Vietnamese",
+  zh: "Chinese",
+  en: "English",
+  fr: "French",
+  de: "German",
+  it: "Italian",
+  ja: "Japanese",
+  ko: "Korean",
+  pt: "Portuguese",
+  ru: "Russian",
+  es: "Spanish",
+  vi: "Vietnamese",
 };
 
-const getSystemMessage = lang => {
+const getSystemMessage = (lang) => {
   const language = languages[lang];
   return {
     role: "system",
@@ -39,13 +39,13 @@ const getSystemMessage = lang => {
         Your knowledge will be plausible based on your education and backstory.
         You will answer all of my questions as this person.
         You will plausibly generate all unknown information.
-        I am now talking to you.
-        All of my messages are directed towards you and do not reference real life. At all time, you should stay in character.
+        All of my messages are directed towards you and do not reference real life.
         You will only reply in ${language}, even if I speak to you in another language. For example, when I ask "How are you?", you should reply "我很好，你呢？".
+        I don't want you to answer like this: {${language}} ({English translation}). Instead, I want you to always answer like this: {${language}}.
         Each reply should be short, between 1 to 2 sentences.
       `,
   };
-}
+};
 
 const chats = {};
 
@@ -53,8 +53,9 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
     });
     return;
   }
@@ -84,11 +85,11 @@ export default async function (req, res) {
       messages,
     });
     const result = response.data.choices[0].message.content;
-    
+
     messages.push({ role: "assistant", content: result });
 
     res.status(200).json({ chatId, result });
-  } catch(error) {
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
